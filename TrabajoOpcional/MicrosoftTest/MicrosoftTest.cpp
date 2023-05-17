52d6f4c0deabc94e253f03490febd95b170fdae6
PablosGon <100775130+PablosGonUMA@users.noreply.github.com>
2023-05-17 20:06:58 +0200

    Añadido un test a la clase MicrosoftTest

--- TrabajoOpcional/MicrosoftTest/MicrosoftTest.cpp
+++ TrabajoOpcional/MicrosoftTest/MicrosoftTest.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include <C:\Users\pablo\Desktop\MPS-TrabajoOpcional\TrabajoOpcional\ClasesAProbar\CollapseLines\CollapseLines.cpp>
+#include "../ClasesAProbar/CollapseLines/CollapseLines.h"
 
+
+using namespace std;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace MicrosoftTest
@@ -9,8 +13,9 @@ namespace MicrosoftTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Should_ThrowException_When_InputIsEmptyString)
 		{
+			Assert::ExpectException<std::out_of_range>([&] { CollapseLines::collapseNewLines(""); });
 		}
 	};
 }
