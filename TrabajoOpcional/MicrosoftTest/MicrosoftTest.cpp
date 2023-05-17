b57ce433849df443ec7c2355b88fe262dfeb77be
PablosGon <100775130+PablosGonUMA@users.noreply.github.com>
2023-05-17 19:50:27 +0200

    creado el proyecto MicrosoftTest

---
+++ TrabajoOpcional/MicrosoftTest/MicrosoftTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MicrosoftTest
+{
+	TEST_CLASS(MicrosoftTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
